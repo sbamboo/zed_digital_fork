26b78357d78c9ca56bbfafd7a6c6e8655db1d9e9
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2025-01-11 22:15:07 +0100

    upload

---
+++ sorted/js/cookies.js
@@ -0,0 +1,28 @@
+const consentBox = document.getElementById("consentBox");
+const acceptBtn = document.querySelector(".consentButton");
+const rejectBtn = document.querySelector(".rejectButton");
+
+acceptBtn.onclick = () => {
+	document.cookie = "CookieBy=GeeksForGeeks; max-age=" +
+		60 * 60 * 24;
+	if (document.cookie) {
+		consentBox.classList.add("hide");
+	} else {
+		alert
+			("Cookie can't be set! Please" +
+			 " unblock this site from the cookie" +
+			 " setting of your browser.");
+	}
+};
+
+rejectBtn.onclick = () => {
+	alert("Cookies rejected. Some functionality may be limited.");
+	consentBox.classList.add("hide");
+};
+
+let checkCookie = document.cookie.indexOf("CookieBy=GeeksForGeeks");
+if (checkCookie !== -1) {
+	consentBox.classList.add("hide");
+} else {
+	consentBox.classList.remove("hide");
+}
